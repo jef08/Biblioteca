08a0b04dd916e1ef3fc6fa9d98a27c18de11c0a7
jef08 <jasfielding@gmail.com>
2025-03-17 12:22:19 +0100

    He añadido paginadores y mejorado los listados

--- index_libros.php
+++ index_libros.php
@@ -17,26 +17,52 @@ if ($_SERVER['REQUEST_METHOD']==='POST') {
     echo "<p>Libro eliminado correctamente";
 }
 
+//pagination//
+$libros = $manager -> getLibros();
+
+$libroLimit = 5;
+$currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$offset = ($currentPage - 1) * $libroLimit;
+$shownLibros = array_slice($libros, $offset, $libroLimit);
+$totalLibros = count($libros);
+$totalPaginas = ceil($totalLibros/$libroLimit);
+
 echo "<br><h2>Listado de Libros</h2>";
-echo '<div class = "libros">';
-        if (count($manager->getLibros()) == 0) {
-            echo '<p>No hay libros registrados</p>';
-        } else {
-            echo "<ul>";
-            foreach ($manager -> getLibros() as $index => $libro) {
-                echo "<li>";
-                echo "<strong>Título</strong>: " . $libro -> getTitulo() . "<br><strong>Autor</strong>: " . $libro-> getAutor() . "<br><strong>Año</strong>: " . $libro -> getAnyo() . "<br><strong>Páginas</strong>: " . $libro -> getPaginas();
-                echo '<form method = "POST" class = "delete-form">';
-                echo '<input type = "hidden" name = "action" value = "delete">';
-                echo '<input type = "hidden" name = "index" value="' . $index . '">';
-                echo '<input type = "hidden" name = "var" value = "' . $libro -> getPaginas() . '">';
-                echo '<button type = "submit" class = "eliminar">Eliminar</button>';
-                echo '</form>';
-                echo '</li>';
-            }
-            echo '</ul>';
-            echo '</div>';
-        }
+if (count($shownLibros) == 0) {
+    echo '<p>No hay libros registrados</p>';
+} else {
+    echo '<div id="book-container">';
+    foreach ($shownLibros as $index => $libro) {
+        echo '<div class="libros">';
+        echo "<strong>Título</strong>: " . $libro->getTitulo() . "<br><strong>Autor</strong>: " . $libro->getAutor() . "<br><strong>Año</strong>: " . $libro->getAnyo() . "<br><strong>Páginas</strong>: " . $libro->getPaginas();
+        echo '<form method="POST" class="delete-form">';
+        echo '<input type="hidden" name="action" value="delete">';
+        echo '<input type="hidden" name="index" value="' . $index . '">';
+        echo '<button type="submit" class="eliminar">Eliminar</button>';
+        echo '</form>';
+        echo '</div>';
+    }
+    echo '</div>';
+}
+
+//pagination//
+echo '<div id = "pagination">';
+if ($currentPage > 1) {
+    echo '<a href="?page=' . ($currentPage - 1) . '" class = "page-symbol">&laquo;</a>';
+}
+
+for ($i = 1; $i <= $totalPaginas; $i++) {
+    if ($i == $currentPage) {
+        echo '<a href="?page=' . $i . '" class="page-number active">' . $i . '</a>';
+    } else {
+        echo '<a href="?page=' . $i . '" class="page-number">' . $i . '</a>';
+    }
+}
+
+if ($currentPage < $totalPaginas) {
+    echo '<a href="?page=' . ($currentPage + 1) . '" class = "page-symbol">&raquo;</a>';
+}
+echo '</div>';
 ?>
 
 </html>
--- index_revistas.php
+++ index_revistas.php
@@ -17,26 +17,53 @@ if ($_SERVER['REQUEST_METHOD']==='POST') {
     echo "<p>Revista eliminado correctamente";
 };
 
+//pagination//
+$revistas = $manager -> getRevistas();
+
+$revistaLimit = 5;
+$currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$offset = ($currentPage - 1) * $revistaLimit;
+$shownRevistas = array_slice($revistas, $offset, $revistaLimit);
+$totalRevistas = count($revistas);
+$totalPaginas = ceil($totalRevistas/$revistaLimit);
+
 echo "<br><h2>Listado de Revistas</h2>";
-            echo '<div class = "revistas">';
-            if (count($manager->getRevistas()) == 0) {
-                echo "<p> No hay revistas registrados</p>";
-            } else {
-                echo "<ul>";
-                foreach ($manager -> getRevistas() as $index => $revista) {
-                    echo "<li>";
-                    echo "<strong>Título</strong>: " . $revista -> getTitulo() . "<br><strong>Autor:</strong> " . $revista-> getAutor() . "<br><strong> Año</strong>: " . $revista -> getAnyo() . "<br><strong>Tipo</strong>: " . $revista -> getTematica();
-                    echo '<form method = "POST" class = "delete-form">';
-                    echo '<input type = "hidden" name = "action" value = "delete">';
-                    echo '<input type = "hidden" name = "index" value="' . $index . '">';
-                    echo '<input type = "hidden" name = "var" value = "' . $revista -> getTematica() . '">';
-                    echo '<button type = "submit" class = "eliminar">Eliminar</button>';
-                    echo '</form>';
-                    echo '</li>';
-                }
-                echo '</ul>';
-                echo '</div>';
-            }
+            
+if (count($shownRevistas) == 0) {
+    echo '<p>No hay revistas registrados</p>';
+} else {
+    echo '<div id="revista-container">';
+    foreach ($shownRevistas as $index => $revista) {
+        echo '<div class="revistas">';
+        echo "<strong>Título</strong>: " . $revista->getTitulo() . "<br><strong>Autor</strong>: " . $revista->getAutor() . "<br><strong>Año</strong>: " . $revista->getAnyo() . "<br><strong>Páginas</strong>: " . $revista->getTematica();
+        echo '<form method="POST" class="delete-form">';
+        echo '<input type="hidden" name="action" value="delete">';
+        echo '<input type="hidden" name="index" value="' . $index . '">';
+        echo '<button type="submit" class="eliminar">Eliminar</button>';
+        echo '</form>';
+        echo '</div>';
+    }
+    echo '</div>';
+}
+
+echo '<div id = "pagination">';
+if ($currentPage > 1) {
+    echo '<a href="?page=' . ($currentPage - 1) . '" class = "page-symbol">&laquo;</a>';
+}
+
+for ($i = 1; $i <= $totalPaginas; $i++) {
+    if ($i == $currentPage) {
+        echo '<a href="?page=' . $i . '" class="page-number active">' . $i . '</a>';
+    } else {
+        echo '<a href="?page=' . $i . '" class="page-number">' . $i . '</a>';
+    }
+}
+
+
+if ($currentPage < $totalPaginas) {
+    echo '<a href="?page=' . ($currentPage + 1) . '" class = "page-symbol">&raquo;</a>';
+}
+echo '</div>';
 ?>
 
 </html>
